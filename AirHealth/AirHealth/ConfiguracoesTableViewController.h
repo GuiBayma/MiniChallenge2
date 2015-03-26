c2f001373a7acc92a2541384fb7bf367b2aa449e
Kaique Damato <kaique_pantosi@hotmail.com>
2015-03-26 08:18:41 -0300

    criação das classes configurações e ficha médica

---
+++ AirHealth/AirHealth/ConfiguracoesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConfiguracoesTableViewController.h
+//  AirHealth
+//
+//  Created by Kaique Damato on 3/25/15.
+//  Copyright (c) 2015 Guilherme Bayma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfiguracoesTableViewController : UITableViewController
+
+@end
