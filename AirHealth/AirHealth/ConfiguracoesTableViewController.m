e0adbdf6006ae3479e116dfa58680222b8b6820a
Guilherme Ferreira de Souza <guilherme.souza94@hotmail.com>
2015-03-26 16:45:17 -0300

    teste

--- AirHealth/AirHealth/ConfiguracoesTableViewController.m
+++ AirHealth/AirHealth/ConfiguracoesTableViewController.m
@@ -18,6 +18,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //teste
+    
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
