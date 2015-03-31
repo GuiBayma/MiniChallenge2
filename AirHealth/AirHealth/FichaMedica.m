2f9c3b247e723183085020bfd0678dd097d8d74b
Guilherme Ferreira de Souza <guilherme.souza94@hotmail.com>
2015-03-31 09:15:38 -0300

    Persistência local

--- AirHealth/AirHealth/FichaMedica.m
+++ AirHealth/AirHealth/FichaMedica.m
@@ -19,12 +19,12 @@
     
     [coder encodeObject:self.altura forKey:@"altura"];
     [coder encodeObject:self.indiceMassaCorporal forKey:@"imc"];
-    [coder encodeObject:self.massaCorporalMagra forKey:@"massaCorporalMagra"];
-    [coder encodeObject:self.percentualGorduraCorporal forKey:@"gorduraCorporal"];
+    //[coder encodeObject:self.massaCorporalMagra forKey:@"massaCorporalMagra"];
+    //[coder encodeObject:self.percentualGorduraCorporal forKey:@"gorduraCorporal"];
     [coder encodeObject:self.peso forKey:@"peso"];
     
-    [coder encodeObject:self.frequenciaCardiaca forKey:@"frequenciaCardiaca"];
-    [coder encodeObject:self.frequenciaRespiratoria forKey:@"frequenciaRespiratoria"];
+    //[coder encodeObject:self.frequenciaCardiaca forKey:@"frequenciaCardiaca"];
+    //[coder encodeObject:self.frequenciaRespiratoria forKey:@"frequenciaRespiratoria"];
     [coder encodeObject:self.pressaoArterialSistolica forKey:@"pressaoSistolica"];
     [coder encodeObject:self.pressaoArterialDiastolica forKey:@"pressaoDiastolica"];
     [coder encodeObject:self.temperaturaCorporal forKey:@"temperaturaCorporal"];
@@ -41,12 +41,12 @@
     
     [self setAltura:[decoder decodeObjectForKey:@"altura"]];
     [self setIndiceMassaCorporal:[decoder decodeObjectForKey:@"imc"]];
-    [self setMassaCorporalMagra:[decoder decodeObjectForKey:@"massaCorporalMagra"]];
-    [self setPercentualGorduraCorporal:[decoder decodeObjectForKey:@"gorduraCorporal"]];
+    //[self setMassaCorporalMagra:[decoder decodeObjectForKey:@"massaCorporalMagra"]];
+    //[self setPercentualGorduraCorporal:[decoder decodeObjectForKey:@"gorduraCorporal"]];
     [self setPeso:[decoder decodeObjectForKey:@"peso"]];
     
-    [self setFrequenciaCardiaca:[decoder decodeObjectForKey:@"frequenciaCardiaca"]];
-    [self setFrequenciaRespiratoria:[decoder decodeObjectForKey:@"frequenciaRespiratoria"]];
+    //[self setFrequenciaCardiaca:[decoder decodeObjectForKey:@"frequenciaCardiaca"]];
+    //[self setFrequenciaRespiratoria:[decoder decodeObjectForKey:@"frequenciaRespiratoria"]];
     [self setPressaoArterialSistolica:[decoder decodeObjectForKey:@"pressaoSistolica"]];
     [self setPressaoArterialDiastolica:[decoder decodeObjectForKey:@"pressaoDiastolica"]];
     [self setTemperaturaCorporal:[decoder decodeObjectForKey:@"temperaturaCorporal"]];
